074b96e5425d3e1003cb167c52e819cd2bc4e443
Ben Sutherland <blender.sleuth@gmail.com>
2021-07-06 15:19:08 +1000

    Updated comments

--- Source/ComputeShaders/Public/RayTracingCS.h
+++ Source/ComputeShaders/Public/RayTracingCS.h
@@ -29,13 +29,13 @@ public:
 		SHADER_PARAMETER_RDG_BUFFER_UAV(RWStructuredBuffer<float2>, RandomBuffer)
 	END_SHADER_PARAMETER_STRUCT()
 
-	//Called by the engine to determine which permutations to compile for this shader
+	// Called by the engine to determine which permutations to compile for this shader
 	static bool ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters)
 	{
 		return IsFeatureLevelSupported(Parameters.Platform, ERHIFeatureLevel::SM5);
 	}
 
-	//Modifies the compilations environment of the shader
+	// Modifies the compilations environment of the shader
 	static void ModifyCompilationEnvironment(const FGlobalShaderPermutationParameters& Parameters, FShaderCompilerEnvironment& OutEnvironment)
 	{
 		FGlobalShader::ModifyCompilationEnvironment(Parameters, OutEnvironment);
